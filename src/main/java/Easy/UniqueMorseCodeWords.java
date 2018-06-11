21b382b86ec0d9fed082c9fddbbfda9b1bdd160f
Triador <triador.sub@yandex.ru>
2018-06-11 13:10:37 +0300

    UniqueMorseCodeWords

---
+++ src/main/java/Easy/UniqueMorseCodeWords.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.HashSet;
+
+public class UniqueMorseCodeWords {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCodes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",
+                ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+                "...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> wordsMorseCode = new HashSet<>();
+        for (String word: words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                sb.append(morseCodes[word.charAt(i) - 'a']);
+            }
+            wordsMorseCode.add(sb.toString());
+        }
+        return wordsMorseCode.size();
+    }
+}
---
+++ src/test/java/Easy/TestUniqueMorseCodeWords.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestUniqueMorseCodeWords {
+
+    @Test
+    public void uniqueMorseRepresentations() {
+        UniqueMorseCodeWords uniqueMorseCodeWords = new UniqueMorseCodeWords();
+        assertEquals(2, uniqueMorseCodeWords.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"}));
+    }
+}
