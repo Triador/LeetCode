c376a8d4c7fe585192d41b5c667b946868bae38d
Triador <triador.sub@yandex.ru>
2018-06-13 02:29:06 +0300

    FlippingAnImage

---
+++ src/main/java/Easy/FlippingAnImage.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            int right = A[i].length - 1;
+            for (int j = 0; j < A[i].length /2; j++) {
+                if (A[i][j] == A[i][right]) {
+                    A[i][j] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                right--;
+            }
+            if (A[i].length % 2 != 0) {
+                A[i][right] ^= 1;
+            }
+        }
+        return A;
+    }
+}
---
+++ src/test/java/Easy/TestFlippingAnImage.java
@@ -0,0 +1,17 @@
+package Easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestFlippingAnImage {
+
+    @Test
+    public void flipAndInvertImage() {
+        FlippingAnImage flippingAnImage = new FlippingAnImage();
+        assertTrue(Arrays.deepEquals(new int[][]{{1,0,0},{0,1,0},{1,1,1}}, flippingAnImage.flipAndInvertImage(new int[][]{{1,1,0},{1,0,1},{0,0,0}})));
+        assertTrue(Arrays.deepEquals(new int[][]{{1,1,0,0},{0,1,1,0},{0,0,0,1},{1,0,1,0}}, flippingAnImage.flipAndInvertImage(new int[][]{{1,1,0,0},{1,0,0,1},{0,1,1,1},{1,0,1,0}})));
+    }
+}
