15b127d4e063f079fd652a4aae1319746127853d
triador <triador.sub@yandex.ru>
2018-06-26 18:06:58 +0300

    ShortestDistanceToACharacter rename variables

--- src/main/java/easy/ShortestDistanceToACharacter.java
+++ src/main/java/easy/ShortestDistanceToACharacter.java
@@ -2,21 +2,17 @@ package easy;
 
 public class ShortestDistanceToACharacter {
     public int[] shortestToChar(String S, char C) {
-        int N = S.length();
-        int[] ans = new int[N];
-        int prev = Integer.MIN_VALUE / 2;
-
-        for (int i = 0; i < N; ++i) {
-            if (S.charAt(i) == C) prev = i;
-            ans[i] = i - prev;
+        int n = S.length();
+        int[] res = new int[n];
+        int pos = -n;
+        for (int i = 0; i < n; ++i) {
+            if (S.charAt(i) == C) pos = i;
+            res[i] = i - pos;
         }
-
-        prev = Integer.MAX_VALUE / 2;
-        for (int i = N-1; i >= 0; --i) {
-            if (S.charAt(i) == C) prev = i;
-            ans[i] = Math.min(ans[i], prev - i);
+        for (int i = n - 1; i >= 0; --i) {
+            if (S.charAt(i) == C)  pos = i;
+            res[i] = Math.min(res[i], Math.abs(i - pos));
         }
-
-        return ans;
+        return res;
     }
 }
